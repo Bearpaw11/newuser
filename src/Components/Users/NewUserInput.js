bc770345d1ff1aabe141f3465a54f2b997ae45a2
Chris Behrens <chrisbehrens@yahoo.com>
2022-01-31 12:08:07 -0700

    feat: added useState and event handlers to the username and age input fields in NewUserInput.js

--- src/Components/Users/NewUserInput.js
+++ src/Components/Users/NewUserInput.js
@@ -1,20 +1,35 @@
-import Card from "../UI/Card"
-import classes from "./NewUserInput.module.css"
-import Button from "../UI/Button"
+import React, {useState} from 'react';
+import Card from "../UI/Card";
+import classes from "./NewUserInput.module.css";
+import Button from "../UI/Button";
 
-const NewUserInput = () => {
+
+const NewUserInput = (props) => {
+
+const [enteredUserName, setEnteredUserName] = useState('');
+const [enteredUserAge, setEnteredUserAge] = useState('');
 
 const formHandler = event => {
   event.preventDefault();
+  console.log(enteredUserAge,enteredUserName)
 }
+
+const userNameChangeHandler = event =>{
+  setEnteredUserName(event.target.value);
+}
+
+const userAgeChangeHandler = event =>{
+  setEnteredUserAge(event.target.value);
+}
+
 
   return (
     <Card className={classes.input}>
       <form onSubmit={formHandler}>
         <label htmlFor="username">Username:</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={userNameChangeHandler} />
         <label htmlFor="age">User Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={userAgeChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
