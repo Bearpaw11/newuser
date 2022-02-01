28118d7819c9f2c096b0555c5b69fda4eabbbc62
Chris Behrens <chrisbehrens@yahoo.com>
2022-01-31 21:46:12 -0700

    feat: added conditional rendering for error modal

--- src/Components/Users/NewUserInput.js
+++ src/Components/Users/NewUserInput.js
@@ -10,15 +10,22 @@ const NewUserInput = (props) => {
 
 const [enteredUserName, setEnteredUserName] = useState('');
 const [enteredUserAge, setEnteredUserAge] = useState('');
+const [error, setError] = useState()
 
 const formHandler = event => {
   event.preventDefault();
   if (enteredUserName.trim().length === 0 || enteredUserAge.trim().length === 0) {
-    console.log('hoping')
+    setError({
+      title: "Invaild input",
+      message: "Please enter a valid name and age ----> No Empty Values"
+    })
     return; 
   }
   if (+enteredUserAge < 1) {
-    console.log('hoping')
+    setError({
+      title: "Invaild age",
+      message: "Please enter a valid age...No Negative ages"
+    })
     return;
   }
   props.onAddUser(enteredUserName,enteredUserAge)
@@ -36,10 +43,13 @@ const userAgeChangeHandler = event =>{
   setEnteredUserAge(event.target.value);
 }
 
+const errorHandler = () => {
+  setError(null);
+};
 
   return (
     <div>
-      <ErrorModal title="an error occured" message="something went wrong dude" />
+      {error && <ErrorModal title={error.title} message={error.message} clearError={errorHandler} />}
       <Card className={classes.input}>
         <form onSubmit={formHandler}>
           <label htmlFor="username">Username:</label>
